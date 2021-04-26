0126391cf099104a9beb669f044ac5921f296540
Nam JaeYoun <siisee11@Nams-Mac-mini.local>
2021-04-26 20:48:06 +0900

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/siisee11/golang-blockchain
+
+go 1.16
+
+require rsc.io/quote v1.5.2 // indirect
---
+++ main.go
@@ -0,0 +1,22 @@
+// main.go
+package main
+
+import (
+	"fmt"
+
+	"github.com/siisee11/golang-blockchain/blockchain"
+)
+
+func main() {
+	chain := blockchain.InitBlockChain()
+
+	chain.AddBlock("First Block after Genesis")
+	chain.AddBlock("second Block after Genesis")
+	chain.AddBlock("Third Block after Genesis")
+
+	for _, block := range chain.Blocks {
+		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
+		fmt.Printf("Data in Block: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+	}
+}
